e9a5bb5d80b779d75ada9397d9536f016c3e868e
maniraj-mm48 <maniraj.mm48@gmail.com>
2026-02-25 13:39:46 -0500

    Initial Commit

---
+++ MyAlarmApp/MyAlarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmAppApp.swift
+//  MyAlarmApp
+//
+//  Created by Maniraj on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MyAlarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
